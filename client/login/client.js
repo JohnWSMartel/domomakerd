69ad0af177f7c4d2a8932694027adbe502d02e29
John Martel (RIT Student) <jwm3464@ad.rit.edu>
2019-03-25 14:37:57 -0400

    Step 22

---
+++ client/login/client.js
@@ -0,0 +1,29 @@
+const handleLogin = (e) => {
+  e.preventDefault();
+  
+  $("#domoMessage").animate({width:'hide'},350);
+  
+  if($("#user").val() == ''||$("#pass").val() == ''){
+    handleError("RAWR! Username or password is empty");
+    return false;
+  }
+  
+  console.log($("input[name=_csrf]").val());
+  
+  sendAjax('POST', $("#loginForm").attr("action"),$("#loginForm").serialize(), redirect);
+  
+  return false;
+};
+
+const handleSignup = (e) => {
+  e.preventDefault();
+  
+  $("#domoMessage").animate({width:'hide'},350);
+  
+  if($("#user").val() == ''||$("#pass").val() == ''||$("#pass2").val() == '') {
+    handleError("RAWR! All fields are required");
+    return false;
+  }
+  
+  
+}
